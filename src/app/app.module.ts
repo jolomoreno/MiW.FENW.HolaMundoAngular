e01c939121bb3d44b5db143369b5d907f606a20e
Jose Lorenzo <jolorenzom@gmail.com>
2018-11-17 00:19:19 +0100

    Enrutado y estilos de links activos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { RootComponent } from './layouts/root/root.component';
+import { HomeComponent } from './layouts/home/home.component';
+import { ServiciosComponent } from './layouts/servicios/servicios.component';
+import { InstalacionesComponent } from './layouts/instalaciones/instalaciones.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
-    RootComponent
+    RootComponent,
+    HomeComponent,
+    ServiciosComponent,
+    InstalacionesComponent
   ],
   imports: [
     BrowserModule,
