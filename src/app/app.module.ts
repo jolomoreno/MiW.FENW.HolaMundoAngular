d8f9dc34ffd6db8ddf891c6e58d0cef0919f9524
Jose Lorenzo <jolorenzom@gmail.com>
2018-11-16 23:28:29 +0100

    Refactor js y css (se ponen todos los archivos en local)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
+import { RootComponent } from './layouts/root/root.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    RootComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RootComponent]
 })
 export class AppModule { }
